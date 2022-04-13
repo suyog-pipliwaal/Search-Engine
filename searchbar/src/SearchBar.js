9975c92941a356cb52009f810398629d4eb09c96
Suyog Pipliwal <suyogpipliwal@gmail.com>
2022-04-13 16:34:51 +0100

    some more changes

--- searchbar/src/SearchBar.js
+++ searchbar/src/SearchBar.js
@@ -46,17 +46,6 @@ function SearchBar({ placeholder}) {
 
 
       </div>
-      {filteredData.length != 0 && (
-        <div className="dataResult">
-          {filteredData.slice(0, 15).map((value, key) => {
-            return (
-              <a className="dataItem" href={value.link} target="_blank">
-                <p>{value.title} </p>
-              </a>
-            );
-          })}
-        </div>
-      )}
     </div>
   );
 }
